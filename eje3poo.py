a726b8387f11bfff8d414511a77146ab547846d2
Jack625a <ktfut15@gmail.com>
2025-09-03 10:19:23 -0400

    SUBCLASES

--- eje3poo.py
+++ eje3poo.py
@@ -22,6 +22,27 @@ class Empleado:
         print(f"Pago de Beneficios del empleado {self.nombre} - {calculoBono+calculoHorasExtra}Bs")
         self.cuenta=self.cuenta+calculoBono+calculoHorasExtra
         print(f" Saldo actual {self.cuenta}Bs Titular: {self.nombre}")
+
+#CREACION DE SUBCLASES
+#class nombreSubclase(ClasePadre):
+class EmpleadoPorHoras(Empleado):
+    #Definir el constructor y realizar la herencias de atributos
+    def __init__(self,nombre,sueldo_base,puesto,horasTrabajadas,cuenta=0,):
+        #Para heredar los atributos metod super()
+        super().__init__(nombre,sueldo_base,puesto,cuenta)
+        self.horasTrabajadas=horasTrabajadas
+    #Acciones de la clase. 
+    def cobrar(self,pago):
+        self.cuenta=self.cuenta+(self.horasTrabajadas*pago)
+        print(f"Pago a realizar: {self.horasTrabajadas*pago}Bs - Saldo: {self.cuenta}Bs")
+
+
+empleado3=EmpleadoPorHoras("Ana",0,"Envios",10)
+empleado3.cobrar(10)
+
+
+
+
 #Paso 4. definir los objetos de la clase
 empleado1=Empleado("Kevin Arroyo",2500,"Administracion")
 empleado2=Empleado("Juan Perez",5000,"Jefe de Planta")

710de3a94e2b1db7fac306da7f4a928e71262661
Jack625a <ktfut15@gmail.com>
2025-10-08 10:16:45 -0400

    Contenedor GRIDVIEW

--- framework/contenedoresTabs/src/pantallas/pantalla1.py
+++ framework/contenedoresTabs/src/pantallas/pantalla1.py
@@ -1,7 +1,40 @@
 import flet as ft
 
 def contenidoPantalla1():
-    return ft.Container(
-        content=ft.Text("Pantalla 1 CONTENIDO"),
-        
+    listaDatos=[]
+    for fotos in range(0,100):
+        listaDatos.append(
+            f"https://picsum.photos/150/150/?{fotos}"
+        )
+
+    print(listaDatos)
+    #CONTENEDOR TIPO GRID (FILAS,COLUMNAS)
+    datos=ft.GridView(
+            spacing=5,
+            expand=1,
+            max_extent=150,
+            run_spacing=10,
+            controls=(
+                ft.Image(
+                src=f"https://picsum.photos/150/150?",
+            ),
+             ft.Image(
+                src=f"https://picsum.photos/150/150?",
+            ),
+             ft.Image(
+                src=f"https://picsum.photos/150/150?",
+            ), ft.Image(
+                src=f"https://picsum.photos/150/150?",
+            ), ft.Image(
+                src=f"https://picsum.photos/150/150?",
+            ), ft.Image(
+                src=f"https://picsum.photos/150/150?",
+            )
     )
+    )
+    
+
+
+    return ft.Container(  
+        content=datos
+        )
